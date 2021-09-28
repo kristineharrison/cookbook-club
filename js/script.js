747984f1c8fce274151a659f9f6f4ce1ef7f03c1
kkochie <kristinekochie@gmail.com>
2021-09-28 14:43:47 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const dinnerPast = document.querySelector(".dinner-past");
+const dinnerCurrent = document.querySelector(".dinner-current");
+const dinnerNext = document.querySelector("dinner-next");
+
+dinnerPast.addEventListener("mouseover", function () {
+    dinnerPast.innerHTML = '<h2>Past</h2>';
+});
+
+dinnerPast.addEventListener("mouseout", function () {
+    dinnerPast.innerHTML = "";
+});
+
+dinnerCurrent.addEventListener("mouseover", function () {
+    dinnerCurrent.innerHTML = '<h2>Current</h2>';
+});
+
+dinnerCurrent.addEventListener("mouseout", function () {
+    dinnerCurrent.innerHTML = "";
+});
+
+dinnerNext.addEventListener("mouseover", function () {
+    dinnerNext.innerHTML = '<h2>Next</h2>';
+});
+
+dinnerNext.addEventListener("mouseout", function () {
+    dinnerNext.innerHTML = "";
+});
